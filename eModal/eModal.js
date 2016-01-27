288451df9966dee609cc62698a4b584d4c32bca6
IChocolateKapa <echochocolatekapa@163.com>
2016-01-27 14:47:16 +0800

    confirm 优化

--- eModal/eModal.js
+++ eModal/eModal.js
@@ -29,10 +29,10 @@ var eModal = {
             sureTitle = this.cfg.sureTitle,
             isDraggable = this.cfg.draggable,
             isClickShade = this.cfg.clickShadeHide,
-            skin = this.cfg.skin;
+            skin = this.cfg.skin,
+            title2 = this.cfg.title;
 
 
-        var title2 = "信息";
         var ss = '<div class="hp-modal ' + skin + '">'
                     + '<p id="hp-title">'+ title2 + '</p>'
                     + '<span>'+ msg + '</span>'
@@ -121,138 +121,126 @@ var eModal = {
             })
     },
     confirm: function (msg, fn, fn2, cfg) {
-        if($(".hp-modal").length > 0 || $(".hp-modal-mask").length > 0) return;
+
+        if ($(".hp-modal").length > 0 || $(".hp-modal-mask").length > 0) return;
+
         this.cfg = {
             width: 300,
-            skin: "0",
-            shade: true,
+            title: "确认",
             sureTitle: "确定",
-            cancleTitle: "取消"
+            cancleTitle: "取消",
+            draggable: false,
+            skin: "skin-blue",
+            shade: true,
+            clickShadeHide: true,
+            sync: true
         };
 
         $.extend(this.cfg, cfg);
 
 
-        var ifShade = this.cfg.shade;
-        var sureTitle = this.cfg.sureTitle;
-        var cancleTitle = this.cfg.cancleTitle;
+        var ifShade = this.cfg.shade,
+            ifSync = this.cfg.sync,
+            sureTitle = this.cfg.sureTitle,
+            cancleTitle = this.cfg.cancleTitle,
+            isDraggable = this.cfg.draggable,
+            isClickShade = this.cfg.clickShadeHide,
+            skin = this.cfg.skin,
+            title2 = this.cfg.title;
 
-        var title2 = "确认框";
-        var ss = "<div class=\"hp-modal\">"
-            + "<p id='hp-title'>"+ title2 + "</p>"
-            + "<span>"+ msg + "</span>"
-            + "<div class='btn-group'>"
-            + "<a href='javascript:void(0)' id='hp-modal-btn-sure'>" + sureTitle + "</a>"
-            + "<a href='javascript:void(0)' id='hp-modal-btn-no'>" + cancleTitle + "</a>"
-            + "</div>"
-            + "</div>";
+
+        var ss = '<div class="hp-modal ' + skin + '">'
+                + '<p id="hp-title">'+ title2 + '</p>'
+                + '<span>'+ msg + '</span>'
+                    + '<div class="btn-group">'
+                        + '<a href="javascript:void(0)" id="hp-modal-btn-sure">' + sureTitle + '</a>'
+                        + '<a href="javascript:void(0)" id="hp-modal-btn-no">' + cancleTitle + '</a>'
+                    + '</div>'
+                + '</div>';
+
 
         var docH = $(window.parent.document).height();
-        if(ifShade){
-            var shade = "<div class='hp-modal-mask'></div>";
-            $("body").prepend(shade);
-            $(ss).appendTo($(".hp-modal-mask"));
-            $(".hp-modal-mask").css({"height": docH});
+
+        if (ifShade) {
+            var shade = '<div class="hp-modal-mask ' + skin + '"></div>';
+            $('body').prepend($(shade));
+            $('.hp-modal-mask').append($(ss)).css({'height': docH});
         } else {
-            $(ss).prependTo($("body"));
+            $(ss).prependTo($('body'));
         }
 
 
-        var eleH = $(".hp-modal").height();
-
-        var winH = $(window.parent.window).height();
-        var topH = winH * 0.5 - 40 + "px";
-        $(".hp-modal").css({
-            width: this.cfg.width,
-            //"margin-left": "-" + this.cfg.width/2 + "px",
-            //"top": topH,
-            //"margin-top": "-" + eleH/2 + "px"
+        $('.hp-modal').css({
+            width: this.cfg.width
         });
 
-        var oDiv=document.getElementById('hp-title');
-
-        oDiv.onmousedown=function (ev)
-        {
-            document.onmousemove=function (ev)
-            {
-                /*获取鼠标相对于浏览器的偏移*/
-                var posX = ev.pageX;
-                var posY = ev.pageY;
-                var titleH = $("#hp-title").height();
-                $(".hp-modal").css({"top": posY+titleH+25 + "px", "left": posX+"px"});
-            };
-
-            document.onmouseup=function ()
-            {
-                document.onmousemove=null;
-                document.onmouseup=null;
-
-                if(oDiv.releaseCapture)
-                {
-                    oDiv.releaseCapture();	//IE
-                }
-            };
-
-            if(oDiv.setCapture)
-            {
-                oDiv.setCapture();	//IE
-            }
-
-            return false;
-        };
-
-
-        if(ifShade){
-            $(".hp-modal-mask").click(function(eve){
-                var event = eve || window.event;
-                var target = event.target || event.srcElement;
-                if($(target).parents().hasClass("hp-modal")){
-                    if(event.preventDefault){
-                        event.preventDefault();
-                    } else{
-                        event.returnValue = false;
-                    }
-                    return false;
-                }
-                $(".hp-modal-mask").remove();
-            })
+        if (isDraggable) {
+            $('#hp-title').css({'cursor': 'move'});
+            var oDiv = document.getElementById('hp-title');
+            this.dragEvent(oDiv);
         }
 
-        $("#hp-modal-btn-sure").click(function(){
-            if(fn){
-                fn();
+        if (ifShade) {
+            if (isClickShade) {
+                $('.hp-modal-mask').click(function (eve) {
+                    var event = eve || window.event;
+                    var target = event.target || event.srcElement;
+                    if ($(target).parents().hasClass('hp-modal')
+                        && $(target).attr('id') != 'hp-modal-btn-sure') {
+                        if (event.preventDefault) {
+                            event.preventDefault();
+                        } else {
+                            event.returnValue = false;
+                        }
+                        return false;
+                    }
+                    $('.hp-modal-mask').remove();
+                    ifSync? fn && fn(): "";
+                })
             }
-            if(ifShade){
-                $(".hp-modal-mask").remove();
+        }
+
+
+        $('#hp-modal-btn-sure').click(function () {
+            if (ifSync) {
+                fn && fn();
+                if (ifShade) {
+                    $(".hp-modal-mask").remove();
+                } else {
+                    $('.hp-modal').remove();
+                }
             } else {
-                $(".hp-modal").remove();
+                if (ifShade) {
+                    $('.hp-modal-mask').remove();
+                } else {
+                    $('.hp-modal').remove();
+                }
+                fn && fn();
             }
+        });
 
-        })
-
-        $("#hp-modal-btn-no").click(function(){
-            if(fn2){
+        $('#hp-modal-btn-no').click(function () {
+            if (fn2) {
                 fn2();
             }
-
-            if(ifShade){
-                $(".hp-modal-mask").remove();
+            if (ifShade) {
+                $('.hp-modal-mask').remove();
             } else {
-                $(".hp-modal").remove();
+                $('.hp-modal').remove();
             }
-        })
+        });
 
 
 
-        var clx = $("<i class=\"closeX\" title=\"关闭此窗口\">X</i>")
-                    .prependTo($(".hp-modal"))
-                    .click(function(){
-                        if(ifShade){
-                            $(".hp-modal-mask").remove();
-                        } else {
-                            $(".hp-modal").remove();
-                        }
-                    })
+        $('<i class="closeX" title="关闭此窗口">X</i>')
+            .prependTo($('.hp-modal'))
+            .click(function () {
+                if(ifShade){
+                    $('.hp-modal-mask').remove();
+                } else {
+                    $('.hp-modal').remove();
+                }
+            })
     },
     prompt: function (msg) {
         //alert(msg);
